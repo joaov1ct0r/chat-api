8e95d896d1d8253bda61f28dee307c8df8c255b4
joaov1ct0r <jov9051@outlook.com>
2023-12-10 15:14:14 -0300

    feat: create token service

---
+++ src/services/user/CreateToken.ts
@@ -0,0 +1,32 @@
+import {UserImp} from "@Interfaces/UserImp";
+import jwt from "jsonwebtoken"
+import ms from "ms";
+
+export interface CreateUserTokenServiceImp {
+    execute(user: UserImp): string
+}
+
+export class CreateUserTokenService implements CreateUserTokenServiceImp {
+    private readonly TOKEN_EXPIRES_IN
+
+    constructor() {
+        const MS = 1000
+        const SECONDS = 60
+        const TEN_MINUTES = 10 * SECONDS * MS
+        this.TOKEN_EXPIRES_IN = TEN_MINUTES
+    }
+
+    public execute(user: UserImp): string {
+        const token: string = jwt.sign(
+            {
+                userId: user.id,
+                userEmail: user.email,
+                username: user.name,
+            },
+            process.env.JWT_TOKEN_SECRET as string,
+            { expiresIn: ms(this.TOKEN_EXPIRES_IN)},
+        )      
+
+        return token
+    }
+}

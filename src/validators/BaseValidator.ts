0f111c97bc9c37ff7ad090df102341f661150740
joaov1ct0r <jov9051@outlook.com>
2023-12-10 15:09:36 -0300

    feat: base validator

---
+++ src/validators/BaseValidator.ts
@@ -0,0 +1,21 @@
+import UserImp from '@Interfaces/UserImp'
+import { SafeParseReturnType, ZodObject, ZodRawShape } from 'zod'
+
+export interface BaseValidatorImp<T> {
+  validate(
+    schema: ZodObject<ZodRawShape>,
+    data: object,
+  ): SafeParseReturnType<T, T>
+}
+
+export default abstract class BaseValidator<T> implements BaseValidatorImp<T> {
+  validate(
+    schema: ZodObject<ZodRawShape>,
+    data: object,
+  ): SafeParseReturnType<T, T> {
+    const result = schema.safeParse(data)
+
+    return result as SafeParseReturnType<T, T>
+  }
+}
+

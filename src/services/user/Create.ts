b227b4a97c44090bf43db2ae2696dddb153d1230
joaov1ct0r <jov9051@outlook.com>
2023-12-10 15:13:57 -0300

    feat: create user service

---
+++ src/services/user/Create.ts
@@ -0,0 +1,34 @@
+import { CreateUserRepositoryImp } from "@Database/repositories/user/Create";
+import { FindUserByEmailRepositoryImp } from "@Database/repositories/user/FindByEmail";
+import {UserImp} from "@Interfaces/UserImp";
+import {BaseService} from "@Services/BaseService";
+import bcrypt from "bcrypt"
+
+export interface CreateUserServiceImp {
+    execute(user: UserImp): Promise<UserImp>
+}
+
+export class CreateUserService extends BaseService implements CreateUserServiceImp {
+    private readonly _createUserRepository: CreateUserRepositoryImp
+    private readonly _findUserByEmailRepository: FindUserByEmailRepositoryImp
+
+    constructor(createUserRepository: CreateUserRepositoryImp, findUserByEmailRepository: FindUserByEmailRepositoryImp) {
+        super()
+        this._createUserRepository = createUserRepository
+        this._findUserByEmailRepository = findUserByEmailRepository
+    }
+
+    public async execute(user: UserImp): Promise<UserImp> {
+        const userAlreadyRegistered = await this._findUserByEmailRepository.execute(user.email)
+
+        if (userAlreadyRegistered) {
+            throw this.badRequest('Usúario já cadastrado!')
+        }
+
+        const salts = 12
+        user.password = bcrypt.hashSync(user.password, salts)
+
+        const createdUser = await this._createUserRepository.execute(user)
+        return createdUser
+    }
+}

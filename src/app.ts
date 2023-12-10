907a5dd5a77f05c9155d49866f4c54445fc30e73
joaov1ct0r <jov9051@outlook.com>
2023-12-10 15:15:06 -0300

    feat: app

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cors, { CorsOptions } from 'cors'
 import userRouter from './routes/userRoutes'
 import swaggerUi from 'swagger-ui-express'
 import swaggerDocs from './swagger.json'
+import { BaseResponse } from '@Interfaces/BaseResponse'
 
 export default class App {
   public readonly server: express.Application
@@ -18,8 +19,6 @@ export default class App {
     const corsOptions: CorsOptions = {
       credentials: true,
       exposedHeaders: ['set-cookie'],
-      origin: ['http://localhost:80'],
-      methods: ['GET', 'POST'],
     }
     this.server.use(cors(corsOptions))
     this.server.use(cookieParser())
@@ -32,17 +31,19 @@ export default class App {
     this.server.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    this.server.use((error: any, req: Request, res: Response) => {
+    this.server.use((error: any, req: Request, res: BaseResponse<any>) => {
       if (error && error.statusCode && error.message) {
         return res.status(error.statusCode).json({
           message: error.message,
           status: error.statusCode,
+          error: error
         })
       }
 
       return res.status(500).json({
         message: 'Internal Server error',
-        statusCode: 500,
+        status: 500,
+        error: error
       })
     })
   }

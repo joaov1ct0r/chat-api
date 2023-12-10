8f22a04929f93a548a1678f4fbab011a54e814cb
joaov1ct0r <jov9051@outlook.com>
2023-12-10 15:13:25 -0300

    feat: authorization middleware

---
+++ src/middlewares/Authorization.ts
@@ -0,0 +1,55 @@
+import { NextFunction, Request, Response } from 'express'
+import BadRequest from '@Errors/BadRequest'
+import Forbidden from '@Errors/Forbidden'
+import jwt, { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken'
+import Unauthorized from '@Errors/Unauthorized'
+import {TokenImp} from '@Interfaces/TokenImp'
+
+export default class Authorization {
+  execute(req: Request, res: Response, next: NextFunction) {
+    try {
+      if (!req.cookies.authorization) {
+        throw new BadRequest('Cookie "authorization" não encontrado!')
+      }
+
+      if (!req.cookies.user) {
+        throw new BadRequest('Cookie "user" não encontrado!')
+      }
+
+      const token: string = req.cookies.authorization.split(' ')[1]
+      let user = req.cookies.user
+      user = JSON.parse(user)
+
+      jwt.verify(
+        token,
+        process.env.JWT_TOKEN_SECRET as string,
+        {
+          ignoreNotBefore: true,
+        },
+        (err, result) => {
+          if (err && err.name === 'TokenExpiredError') {
+            throw new Forbidden('Token expirado, faça login novamente!')
+          }
+          if (err && err.name === 'JsonWebTokenError') {
+            throw new Unauthorized(err.message)
+          }
+          if (err && err.name !== 'JsonWebTokenError' && err.name !== 'TokenExpiredError') {
+            throw new Unauthorized('Erro na autenticação do token')
+          }
+
+          req.token = result as TokenImp
+          req.user = user
+          next()
+        },
+      )
+    } catch (error: any) {
+      if (error && error.statusCode && error.message) {
+        return res
+          .status(error.statusCode)
+          .json({ status: error.statusCode, message: error.message })
+      }
+
+      return res.status(500).json({ message: 'Erro interno', status: 500 })
+    }
+  }
+}

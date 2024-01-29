4ca694f1d7561eef0591a12f46fd51ea79440c92
joaov1ct0r <jov9051@outlook.com>
2024-01-29 12:20:49 -0300

    test: create token service

--- src/services/user/CreateToken.spec.ts
+++ src/services/user/CreateToken.spec.ts
@@ -1,5 +1,29 @@
-import { describe, it } from '@jest/globals'
+import { afterAll, describe, expect, it, jest } from '@jest/globals'
+import { CreateUserTokenService } from '@Services/user/CreateToken'
+import jwt from 'jsonwebtoken'
 
 describe('create token service [unit]', () => {
-  it.todo('should be able to create token')
+  const sut = new CreateUserTokenService()
+  const jwtSpy = jest.spyOn(jwt, 'sign')
+
+  afterAll(() => {
+    jest.restoreAllMocks()
+  })
+
+  it('should be able to create token', () => {
+    jwtSpy.mockImplementationOnce(() => {
+      return 'token'
+    })
+
+    const token = sut.execute({
+      createdAt: new Date(),
+      dateBirth: new Date(),
+      email: 'any_email@mail.com',
+      id: 'any_id',
+      name: 'any_name',
+      updatedAt: null,
+    })
+
+    expect(token).toEqual('token')
+  })
 })

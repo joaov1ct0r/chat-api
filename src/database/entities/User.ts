b106c95581a429bafdbadabcfe073c2f129a7cc9
joaov1ct0r <jov9051@outlook.com>
2024-01-29 10:46:38 -0300

    refactor: added userimp to db entity User

--- src/database/entities/User.ts
+++ src/database/entities/User.ts
@@ -1,34 +1,46 @@
-import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm'
+import { UserImp } from '@Entities/User'
+import { Entity, Column, PrimaryColumn } from 'typeorm'
 
-@Entity('users')
-class User extends BaseEntity {
-  @PrimaryGeneratedColumn()
-  id!: string
+@Entity()
+class User implements UserImp {
+  @PrimaryColumn()
+  id: string
 
-  @Column({
-    unique: true,
-    length: 100,
-    nullable: false,
-  })
-  email!: string
+  @Column()
+  email: string
 
-  @Column({
-    length: 100,
-    nullable: false,
-  })
-  name!: string
+  @Column()
+  name: string
 
-  @Column({
-    length: 10,
-    nullable: false,
-  })
-  dateBirth!: string
+  @Column()
+  dateBirth: Date
 
-  @Column({
-    length: 250,
-    nullable: false,
-  })
-  password!: string
+  @Column()
+  password: string
+
+  @Column()
+  createdAt: Date
+
+  @Column()
+  updatedAt: Date | null
+
+  constructor({
+    dateBirth,
+    email,
+    id,
+    name,
+    password,
+    createdAt,
+    updatedAt,
+  }: UserImp) {
+    this.dateBirth = dateBirth
+    this.email = email
+    this.id = id
+    this.name = name
+    this.password = password
+    this.createdAt = createdAt
+    this.updatedAt = updatedAt
+  }
 }
 
 export default User

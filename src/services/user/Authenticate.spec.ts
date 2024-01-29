3e7251bfbaf86ba93906d9d9d38046c9a53bb69a
joaov1ct0r <jov9051@outlook.com>
2024-01-29 10:56:30 -0300

    test: authenticate user service

---
+++ src/services/user/Authenticate.spec.ts
@@ -0,0 +1,7 @@
+import { describe, it } from '@jest/globals'
+
+describe('authenticate user service [unit]', () => {
+  it.todo('should throw bad request exception when user isnt found')
+  it.todo('should throw bad request exception when passwords isnt equal')
+  it.todo('should be able to authenticate user')
+})
